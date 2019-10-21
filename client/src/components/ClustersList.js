fc4fb3161dfbc125316d21284033838645da0553
ngoan98tv <ngoan98travinh@gmail.com>
2019-10-21 09:36:24 +0700

    update preview clusters

--- client/src/components/ClustersList.js
+++ client/src/components/ClustersList.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { Paper } from '@material-ui/core';
+import { Paper, Grid, Typography, Button } from '@material-ui/core';
 
 const properties = ['count', 'min', 'max', 'mean', 'freq', 'top', 'unique'];
 
 function ClustersList({clusters}) {
-    return(
-        <Paper style={{ padding: 12 }}>
-
-        </Paper>
-    );
+    return(<div>
+        {clusters.map(
+            (cluster, index) => (
+                <Paper style={{ padding: 12, marginBottom: 16 }}>
+                    <Typography variant="h3">{index + 1}</Typography>
+                    <Grid 
+                        container
+                        direction="row"
+                        spacing={2}
+                        wrap="nowrap"
+                        style={{
+                            overflowX: 'auto'
+                        }}>
+                            {Object.keys(cluster).map(
+                                field => (
+                                    <Grid item style={{ minWidth: 160 }}>
+                                        <Typography style={{ fontWeight: 'bold' }}>{field}</Typography>
+                                        {properties.map(
+                                            proper => cluster[field][proper] 
+                                                ? <Typography style={{ fontSize: '12px' }}>{proper}: {cluster[field][proper]}</Typography> 
+                                                : ''
+                                        )}
+                                        <Button color="primary" size="small">Tải xuống</Button>
+                                    </Grid>
+                                )
+                            )}
+                    </Grid>
+                </Paper>
+            )
+        )}
+    </div>);
 }
 
 export default ClustersList;
